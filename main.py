0ca2c376fc2009457173f6d37c78cead17c34f7d
lihu <ccblihu@126.com>
2024-09-21 10:14:27 +0800

    全部子层

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from caffe2.python.helpers.dropout import dropout
+from numpy.array_api import positive
 from torch.nn.functional import linear
+
+from single_test.test_Generator import d_model
 
 
 class EncoderDecoder(nn.Module):
@@ -153,10 +156,45 @@ class MutiHeadedAttention(nn.Module):
 """
 Feed Forward Network
 """
+
+
 class PositionwiseFeedForward(nn.Module):
-    def __init__(self):
-        super.__init__()
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super().__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(p=dropout)
 
-    def forward(self):
-        return null
+    def forward(self, x):
+        return self.w_2(self.dropout(self.w_1(x).relu()))
 
+
+"""
+Embedding
+"""
+class Embeddings(nn.Module):
+    def __init__(self, n_vocab, d_model):
+        super().__init__()
+        self.embed = nn.Embedding(n_vocab, d_model)
+    def forward(self, x):
+        return self.embed(x) * math.sqrt(self.d_model)
+
+
+"""
+Position Encoding
+"""
+class PositionEncoding(nn.Module):
+    def __init__(self, d_model, dropout, max_len=5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.squeeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:, :x.size[1]].requires_grad_(False)
+        return self.dropout(x)
